55fc2374721d52b31c5f2aa5e41b2eeaa24019aa
Zach Parker <parkerz410@gmail.com>
2019-05-20 19:23:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,9 @@
 def line (katz_deli)
   katz_deli = []
-  if katz_deli.length != 0
-    katz_deli.each_with_index do |name, number|
-    puts "The line is currently: #{number + 1}. #{name}"
-    end
-  else
+  if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    katz_deli.each_with_index do |name, number|
+    puts "The line is currently: #{number.to_i + 1}. #{name}"
   end
 end
