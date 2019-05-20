567c92469e100ddc2d840dcf28e93b720697544f
Zach Parker <parkerz410@gmail.com>
2019-05-20 20:21:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,17 @@
-require "pry"
+
 
 def line (array)
   newarray = []
   if array.length == 0
     puts "The line is currently empty."
   else
-
     array.each.with_index(1) do |name, number|
       newarray.push("#{number}. #{name}")
-    end
-#binding.pry
     puts "The line is currently: #{newarray.join(" ")}"
   end
+end
+
+def take_a_number (array, name)
+  array.push(name)
+  puts "Welcome, #{name}. You are number #{name[]} in line."
 end
