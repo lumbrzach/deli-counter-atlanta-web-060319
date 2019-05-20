e56a6cde8d383a856dc053876456968870656921
Zach Parker <parkerz410@gmail.com>
2019-05-20 19:40:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,8 +3,8 @@ def line (array)
   if array.length == 0
     puts "The line is currently empty."
   else
-    array.each_with_index do |name, number|
-      array.push("#{number+1}. #{name}")
+    array.each.with_index(0) do |name, number|
+      array.push("#{number}. #{name}")
     end
     puts "The line is currently: #{array.join(" ")}"
   end
