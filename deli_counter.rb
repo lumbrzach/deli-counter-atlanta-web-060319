2546adcfac4cf6880f669614035835576888abf2
Zach Parker <parkerz410@gmail.com>
2019-05-20 19:58:25 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,8 +5,8 @@ def line (array)
   if array.length == 0
     puts "The line is currently empty."
   else
+    binding.pry
     array.each.with_index(0) do |name, number|
-binding.pry
       array.push("#{number}. #{name}")
     end
 
