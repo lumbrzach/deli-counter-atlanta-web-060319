598cf6f99123b5bb6470f5f00a49c8e556bff9b7
Zach Parker <parkerz410@gmail.com>
2019-05-20 20:00:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,15 @@
 require "pry"
 
 def line (array)
-  array = []
+  newarray = []
   if array.length == 0
     puts "The line is currently empty."
   else
-    binding.pry
-    array.each.with_index(0) do |name, number|
-      array.push("#{number}. #{name}")
-    end
 
+    array.each.with_index(0) do |name, number|
+      newarray.push("#{number}. #{name}")
+    end
+binding.pry
     puts "The line is currently: #{array.join(" ")}"
   end
 end
