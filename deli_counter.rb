c353605f9289d765c3ebab8617ed82711360aac1
Zach Parker <parkerz410@gmail.com>
2019-05-20 19:12:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ def line (katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.collect.each_with_index do |name, number|
+    katz_deli.each_with_index do |name, number|
       puts "The line is currenlty: #{number + 1}. #{name}"
     end
   end
